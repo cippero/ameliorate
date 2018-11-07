adf5aaf24e2c3b2b47d5c04f91166467ba5d13a2
cippero <gilwein@gmail.com>
2018-11-07 14:29:04 -0800

    hooked up docker

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,41 @@
-const express    = require('express');
-const bodyParser = require('body-parser');
-const morgan     = require('morgan');
-const cors       = require('cors');
-const app        = express();
+const express               = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const bodyParser            = require('body-parser');
+const morgan                = require('morgan');
+const cors                  = require('cors');
+const app                   = express();
 
+// GraphQL/Apollo ////////////////////////////////////////////////
+const typeDefs = gql`
+	type Query {
+        hello: String,
+        five: Int
+	}
+`;
+
+const resolvers = {
+	Query: {
+        hello: () => 'Hello world!',
+        five: () => 5
+	}
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Middleware ////////////////////////////////////////////////////
+server.applyMiddleware({ app });
 app.use(morgan('combined'));
 app.use(cors());
 app.use(bodyParser.json());
 
+// Routes Handling ///////////////////////////////////////////////
 app.get('/', (req, res) => { res.send('Helloooooo')});
 
+// Port Setup ////////////////////////////////////////////////////
 const port = 8080;
-app.listen(process.env.PORT || port, () => console.log(`Listening on port ${process.env.PORT || port}`));
+app.listen(process.env.PORT || port, () =>
+    console.log(`🚀 Server ready at http://localhost:${process.env.PORT || port}. Access GraphQL at http://localhost:${process.env.PORT || port}${server.graphqlPath}`)
+);
+
+
+// app.listen(process.env.PORT || port, () => console.log(`Listening on port ${process.env.PORT || port}`));

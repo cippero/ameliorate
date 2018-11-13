1292f0d35a77bcf92d8c1f9dda676866229cd99e
cippero <gilwein@gmail.com>
2018-11-12 16:57:08 -0800

    setting up graphql schema

--- backend/schema.js
+++ backend/schema.js
@@ -2,15 +2,46 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 	type Query {
-		hello: String,
-		five: Int
+		id: ID,
+		fname: String,
+		lname: String,
+		email: String,
+		joined: String,
+		stats: UserStats
+	}
+
+	type UserStats {
+		userid: ID,
+		entrydate: String,
+		waterintake: Int,
+		weightpounds: Int,
+		sleepamount: Int,
+		sleepquality: Int,
+		exercisetype: String,
+		exerciselength: Int,
+		exerciseintensity: Int,
+		overallfeeling: Int
 	}
 `;
 
 const resolvers = {
 	Query: {
-		hello: () => 'Hello world!',
-		five: () => 5
+		id: () => 1,
+		fname: () => 'Eddie',
+		lname: () => 'Murphy',
+		email: () => 'eddie@murphy.com',
+		joined: () => '2017-05-14',
+		stats: () => ({
+			entrydate: () => '2017-3-12',
+			waterintake: () => 5,
+			weightpounds: () => 150,
+			sleepamount: () => 7,
+			sleepquality: () => 5,
+			exercisetype: () => 'running',
+			exerciselength: () => 60,
+			exerciseintensity: () => 7,
+			overallfeeling: () => 6
+		})
 	}
 };
 
